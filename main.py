9740020602eb23bd12a2828815ca96e5bfdf4ead
Jack Koloskus <jack@kolosk.us>
2019-07-26 11:51:54 -0400

    look it kinda works!

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from channel import Channel
 
-channel1 = Channel('computer-graphics-jjpidmjvw40')
+channel = Channel('computer-graphics-jjpidmjvw40')
+
+print("title:", channel.title)
+print("by:", channel.user.username)
+for block in channel.contents:
+    print(block.base_class, block.title)

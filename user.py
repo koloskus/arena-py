ab63c656ead2be326d0e2011151c6d62f99f6298
Jack Koloskus <jack@kolosk.us>
2019-07-26 15:00:14 -0400

    updated user.py to use X.get('Y') format

--- user.py
+++ user.py
@@ -2,29 +2,29 @@ import requests
 
 class User:
     def __init__(self, u):
-        self.id = u['id']
-        self.created_at = u['created_at']
-        self.slug = u['slug']
-        self.username = u['username']
-        self.first_name = u['first_name']
-        self.last_name = u['last_name']
-        self.avatar = u['avatar']
-        self.avatar_image = u['avatar_image']
-        self.channel_count = u['channel_count']
-        self.following_count = u['following_count']
-        self.profile_id = u['profile_id']
-        self.follower_count = u['follower_count']
-        self.initials = u['initials']
-        self.can_index = u['can_index']
-        self.metadata = u['metadata']
-        self.is_premium = u['is_premium']
-        self.is_exceeding_private_connections_limit = u['is_exceeding_private_connections_limit']
-        self.is_confirmed = u['is_confirmed']
-        self.is_pending_reconfirmation = u['is_pending_reconfirmation']
-        self.is_pending_confirmation = u['is_pending_confirmation']
-        self.badge = u['badge']
-        self.base_class = u['base_class']
-        self.user_class = u['class']
+        self.id = u.get('id')
+        self.created_at = u.get('created_at')
+        self.slug = u.get('slug')
+        self.username = u.get('username')
+        self.first_name = u.get('first_name')
+        self.last_name = u.get('last_name')
+        self.avatar = u.get('avatar')
+        self.avatar_image = u.get('avatar_image')
+        self.channel_count = u.get('channel_count')
+        self.following_count = u.get('following_count')
+        self.profile_id = u.get('profile_id')
+        self.follower_count = u.get('follower_count')
+        self.initials = u.get('initials')
+        self.can_index = u.get('can_index')
+        self.metadata = u.get('metadata')
+        self.is_premium = u.get('is_premium')
+        self.is_exceeding_private_connections_limit = u.get('is_exceeding_private_connections_limit')
+        self.is_confirmed = u.get('is_confirmed')
+        self.is_pending_reconfirmation = u.get('is_pending_reconfirmation')
+        self.is_pending_confirmation = u.get('is_pending_confirmation')
+        self.badge = u.get('badge')
+        self.base_class = u.get('base_class')
+        self.user_class = u.get('class')
 
 def getUser(slug):
     r = requests.get('http://api.are.na/v2/users/' + slug)

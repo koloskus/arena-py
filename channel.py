cbafefff7187eb36e75f10cbcc2e6a1975fd9951
Jack Koloskus <jack@kolosk.us>
2019-07-26 15:35:07 -0400

    cleaned up and separated http request into function

--- channel.py
+++ channel.py
@@ -3,16 +3,14 @@ import user
 import block
 
 class Channel:
-    def __init__(self, slug):
-        r = requests.get('http://api.are.na/v2/channels/' + slug)
-        c = r.json()
-        self.title = c['title']
+    def __init__(self, c):
         self.id = c['id']
-        self.created = c['created_at']
-        self.updated = c['updated_at']
+        self.title = c['title']
+        self.created_at = c['created_at']
+        self.updated_at = c['updated_at']
         self.added_to_at = c['added_to_at']
-        self.is_published = c['published']
-        self.is_open = c['open']
+        self.published = c['published']
+        self.open = c['open']
         self.collaboration = c['collaboration']
         self.collaborator_count = c['collaborator_count']
         self.slug = c['slug']
@@ -20,18 +18,23 @@ class Channel:
         self.kind = c['kind']
         self.status = c['status']
         self.user_id = c['user_id']
+        # dict
         self.manifest = c['manifest']
-        self.contents = []
-        for channel_block in c['contents']:
-            self.contents.append(block.Block(channel_block))
+        self.contents = c['contents']
         self.base_class = c['base_class']
-        #self.page = c['page']
-        #self.per = c['per']
+        self.page = c['page']
+        self.per = c['per']
         self.collaborators = c['collaborators']
-        self.followers = c['follower_count']
+        self.follower_count = c['follower_count']
         self.share_link = c['share_link']
+        # dict
         self.metadata = c['metadata']
         self.class_name = c['class_name']
         self.can_index = c['can_index']
         self.nsfw = c['nsfw?']
+        # dict
         self.user = user.User(c['user'])
+
+def getChannel(slug):
+    r = requests.get('http://api.are.na/v2/channels/' + slug)
+    return Channel(r.json())

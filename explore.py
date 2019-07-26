e15caa26b541e40dce71fdb4bdb34d8f77af9c7e
Jack Koloskus <jack@kolosk.us>
2019-07-26 18:24:10 -0400

    ad .get() format to explore, add getAll function, store channels as list of slugs for now

--- explore.py
+++ explore.py
@@ -1,15 +1,26 @@
+import requests
 import user
 import channel
 import block
 
 class All:
     def __init__(self, a):
-        self.term = a['term']
-        self.per = a['per']
-        self.current_page = a['current_page']
-        self.total_pages = a['total_pages']
-        self.length = a['length']
-        self.authenticated = a['authenticated']
-        self.channels = a['channels']
-        self.blocks = a['blocks']
-        self.users = a['users']
+        self.term = a.get('term')
+        self.per = a.get('per')
+        self.current_page = a.get('current_page')
+        self.total_pages = a.get('total_pages')
+        self.length = a.get('length')
+        self.authenticated = a.get('authenticated')
+        self.channels = []
+        for ch in a.get('channels'):
+            # if (ch.get('kind') == 'default'):
+            self.channels.append(ch.get('slug'))
+        print(self.channels)
+        self.blocks = a.get('blocks')
+        self.users = a.get('users')
+
+def getAll(terms):
+    print('Getting http://api.are.na/v2/channels/' + terms)
+    r = requests.get('http://api.are.na/v2/channels/' + terms)
+    print(r)
+    return All(r.json())

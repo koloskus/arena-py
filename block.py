3a94033dbaff9337a0764dd591ef73c07475fea6
Jack Koloskus <jack@kolosk.us>
2019-07-26 14:59:45 -0400

    updated block.py to use X.get('Y') format

--- block.py
+++ block.py
@@ -4,27 +4,28 @@ import image
 
 class Block():
     def __init__(self, b):
-        self.id = b['id']
-        self.title = b['title']
-        self.updated_at = b['updated_at']
-        self.created_at = b['created_at']
-        self.metadata = b['metadata']
-        self.base_class = b['base_class']
-        if self.base_class != 'Channel':
-            self.state = b['state']
-            self.comment_count = b['comment_count']
-            self.generated_title = b['generated_title']
-            self.content_html = b['content_html']
-            self.description_html = b['description_html']
-            self.visibility = b['visibility']
-            self.content = b['content']
-            self.description = b['description']
-            self.source = b['source']
-            self.image = image.Image(b['image'])
-            self.embed = b['embed']
-            self.attachment = b['attachment']
+        self.id = b.get('id')
+        self.title = b.get('title')
+        self.updated_at = b.get('updated_at')
+        self.created_at = b.get('created_at')
+        self.metadata = b.get('metadata')
+        self.base_class = b.get('base_class')
+        self.block_class = b.get('class')
+        # if self.base_class != 'Channel':
+        self.state = b.get('state')
+        self.comment_count = b.get('comment_count')
+        self.generated_title = b.get('generated_title')
+        self.content_html = b.get('content_html')
+        self.description_html = b.get('description_html')
+        self.visibility = b.get('visibility')
+        self.content = b.get('content')
+        self.description = b.get('description')
+        self.source = b.get('source')
+        if (self.block_class == 'Image'):
+            self.image = image.Image(b.get('image'))
+        self.embed = b.get('embed')
+        self.attachment = b.get('attachment')
         #named 'block_class' because of built-in conflict with .class
-        self.block_class = b['class']
-        self.user = user.User(b['user'])
-        #self.connections = b['connections']
+        self.user = user.User(b.get('user'))
+        #self.connections = b.get('connections')
         # connections doesn't seem to exist in a block when gathered from Channel['Contents']

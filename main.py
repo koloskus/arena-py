f7744a1724dc160682ed7625915323e107380707
Jack Koloskus <jack@kolosk.us>
2019-07-29 09:24:01 -0400

    added some sample code

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from channel import Channel
+import channel
+import explore
+import requests
+import user
+import block
 
-channel = Channel('computer-graphics-jjpidmjvw40')
+c = channel.getChannel('computer-graphics-jjpidmjvw40')
 
-print("title:", channel.title)
-print("by:", channel.user.username)
-for block in channel.contents:
-    print(block.base_class, block.title)
+print("Blocks in channel " + c.slug + ":")
+for blk in c.contents:
+    b = block.Block(blk)
+    print(b.block_class + ":")
+    print(b.title)
+    if b.block_class == 'Image':
+        print("     " + b.image.filename)
+    print(" ")
